d8d697236a615ad9f46f83c27e7c349fc079185d
jruanova <jorge.r@saffire.com>
2024-12-23 16:47:36 -0300

    env fix

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-host : 'localhost',
-user: 'root',
-password: '',
+host :process.env.DB_HOST,
+user :process.env.DB_USER,
+password :process.env.DB_PASSWORD,
+database :process.env.DB_NAME,
 port: 3307,
-database: 'central-reservas',
-waitForConnection: true,
+waitForConnections: true,
 connectionLimit: 10,
 queueLimit : 0
 
