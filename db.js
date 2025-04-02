1b9d18a9b7a82333515318d6e583ad5f949b14bc
Jorge Ruanova <jruanova.dev@gmail.com>
2025-04-02 16:24:34 -0300

    bak updated

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ host :process.env.DB_HOST,
 user :process.env.DB_USER,
 password :process.env.DB_PASSWORD,
 database :process.env.DB_NAME,
-port: 3307,
+port: process.env.DB_PORT,
 waitForConnections: true,
 connectionLimit: 10,
 queueLimit : 0
