69f2ae5af6b505e293ce54b85460300d0df21180
Jorge Ruanova <jruanova.dev@gmail.com>
2025-04-02 17:34:05 -0300

    listen de express 7

--- db.js
+++ db.js
@@ -12,4 +12,11 @@ queueLimit : 0
 
 });
 
+console.log("🔍 DB_HOST:", process.env.DB_HOST);
+console.log("🔍 DB_USER:", process.env.DB_USER);
+console.log("🔍 DB_PASSWORD:", process.env.DB_PASSWORD ? "CARGADO" : "NO CARGADO");
+console.log("🔍 DB_NAME:", process.env.DB_NAME);
+console.log("🔍 DB_PORT:", process.env.DB_PORT);
+
+
 module.exports = pool;
