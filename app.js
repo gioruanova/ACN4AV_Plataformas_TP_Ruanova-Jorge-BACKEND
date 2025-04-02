0d5afd82fce9c8c494614bf1edfebcf0ecc4d758
Jorge Ruanova <jruanova.dev@gmail.com>
2025-04-02 16:56:14 -0300

    test 3

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT;
-
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
