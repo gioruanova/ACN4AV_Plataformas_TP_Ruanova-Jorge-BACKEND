953d28c3668bd76f12803f3bdc19c08c62f88185
Jorge Ruanova <jruanova.dev@gmail.com>
2025-04-02 17:29:22 -0300

    listen de express 5

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-host :process.env.DB_HOST,
-user :process.env.DB_USER,
-password :process.env.DB_PASSWORD,
-database :process.env.DB_NAME,
-port: process.env.DB_PORT,
-waitForConnections: true,
-connectionLimit: 10,
-queueLimit : 0
-
+  host: process.env.MYSQLHOST,  // Cambiado de DB_HOST a MYSQLHOST
+  user: process.env.MYSQLUSER,  // Cambiado de DB_USER a MYSQLUSER
+  password: process.env.MYSQLPASSWORD,  // Cambiado de DB_PASSWORD a MYSQLPASSWORD
+  database: process.env.MYSQLDATABASE,  // Cambiado de DB_NAME a MYSQLDATABASE
+  port: process.env.MYSQLPORT || 3306,  // Cambiado de DB_PORT a MYSQLPORT
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
